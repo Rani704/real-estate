4e6dbc06bb079cb08a661dcd00e299844af7ea0c
Rani <rani7042003@gmail.com>
2024-03-27 11:16:49 +0530

    first commit

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Companies.css'
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            <img src="./prologis.png" alt="logo" />
+            <img src="./tower.png" alt="logo" />
+            <img src="./equinix.png" alt="logo" />
+            <img src="./realty.png" alt="logo" />
+        </div>
+    </section>
+  )
+}
+
+export default Companies
